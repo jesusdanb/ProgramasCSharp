fd2a3ddc653bac8cc43b1b52ff2eabc721b91628
jesusdanb <89554872+jesusdanb@users.noreply.github.com>
2024-10-11 20:19:39 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkBlue; 
+Console.Clear();
+Console.WriteLine("Holaaaaaaa");
+Console.WriteLine("Cambiando el color de la consola y la letra");
